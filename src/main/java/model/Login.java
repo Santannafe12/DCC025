ba4f593a326880e4ff022761d939aa3092c5dd01
Santannafe <santannafe12@gmail.com>
2024-09-28 17:53:47 -0300

    feat: validacao-login

---
+++ src/main/java/model/Login.java
@@ -0,0 +1,42 @@
+package model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Login {
+  private Map<String, String> usuarios;
+
+    public Login() {
+        usuarios = new HashMap<>();
+        
+        // Dados fictícios de usuários (email, senha) para simular login, remover depois
+        usuarios.put("joao@email.com", "12345");
+        usuarios.put("maria@email.com", "senha123");
+    }
+
+    public boolean validarLogin(String email, String senha) {
+        if (usuarios.containsKey(email)) {
+            String senhaArmazenada = usuarios.get(email);
+            if (senhaArmazenada.equals(senha)) {
+                System.out.println("Login bem-sucedido!");
+                return true;
+            } else {
+                System.out.println("Senha incorreta!");
+                return false;
+            }
+        } else {
+            System.out.println("Usuário não encontrado!");
+            return false;
+        }
+    }
+
+    // Método para adicionar novos usuários ao sistema, remover depois
+    public void adicionarUsuario(String email, String senha) {
+        if (!usuarios.containsKey(email)) {
+            usuarios.put(email, senha);
+            System.out.println("Usuário adicionado com sucesso!");
+        } else {
+            System.out.println("Usuário já existe!");
+        }
+    }
+}
